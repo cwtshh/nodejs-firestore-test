c9bb6ba2d828c0a7faa22032e5f7b8b5d79679ca
cwtshh <gucosta1719@gmail.com>
2024-04-26 22:19:52 -0300

    adding comments

--- controllers/archiveController.js
+++ controllers/archiveController.js
@@ -1,10 +1,9 @@
-const multer = require('multer');
 const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
 const { initializeApp } = require('firebase/app');
 const { exec } = require('child_process');
 const fs = require('fs');
-
-const firebaseConfig = {
+ 
+const firebaseConfig = { // configuração do firebase
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECTID,
@@ -12,19 +11,24 @@ const firebaseConfig = {
     messagingSenderId: process.env.MESSAGINGSENDERID,
     appId: process.env.APPID
 };
-initializeApp(firebaseConfig);
-const storage = getStorage();
+initializeApp(firebaseConfig); // inicializa o firebase
+const storage = getStorage(); // inicializa o storage
 
 const upload_file = async(req, res) => {
     try{
-        const file = req.file;
-        const storageRef = ref(storage, `files/${file.originalname} - ${new Date().toISOString()}`);
-        const metaData = {
+        const file = req.file; // recebe o arquivo em form-data
+        if(!file) { // verifica se o arquivo foi enviado
+            return res.status(400).json({
+                message: 'No file uploaded'
+            });
+        }
+        const storageRef = ref(storage, `files/${file.originalname} - ${new Date().toISOString()}`); // cria a referência para o arquivo
+        const metaData = { // metadados do arquivo
             contentType: file.mimetype
         };
-        const snapshot = await uploadBytesResumable(storageRef, file.buffer, metaData);
-        const downloadURL = await getDownloadURL(snapshot.ref);
-        console.log('File uploaded to', downloadURL);
+        const snapshot = await uploadBytesResumable(storageRef, file.buffer, metaData); // faz o upload do arquivo
+        const downloadURL = await getDownloadURL(snapshot.ref); // pega a URL do arquivo
+        console.log('File uploaded to', downloadURL); // loga a URL do arquivo
         res.status(200).json({
             message: 'File uploaded successfully',
             url: downloadURL,
@@ -41,24 +45,25 @@ const upload_file = async(req, res) => {
 };
 
 const run_python_code = async(req, res) => {
-    const file =  req.file;
-    if(!file) {
+    const file =  req.file; // recebe o arquivo em form-data
+    if(!file) { // verifica se o arquivo foi enviado
         return res.status(400).json({
             message: 'No file uploaded'
         });
     }
-    const pythonCode = fs.readFileSync(file.path, 'utf8');
-    console.log(pythonCode)
-    exec(`python ${file.path}`, (error, stdout, stderr) => {
-        if(error) {
+    const pythonCode = fs.readFileSync(file.path, 'utf8'); // lê o arquivo
+    console.log(pythonCode) // loga o código
+    exec(`python ${file.path}`, (error, stdout, stderr) => { // executa o código python
+        if(error) { // verifica se houve erro
             console.log(`error: ${error.message}`);
             res.status(400).json({
                 message: 'Error running python code'
             });
             return;
         }
-        console.log(`Saída: ${stdout}`);
-        res.send(stdout);
+        console.log(`Saída: ${stdout}`); // loga a saída
+        res.send(stdout); // envia a saída
+        fs.unlinkSync(file.path); // deleta o arquivo
     });
 };
 
--- controllers/groupController.js
+++ controllers/groupController.js
@@ -4,14 +4,12 @@ const db = admin.firestore();
 
 const get_groups = async(req, res) => {
     const groups = await db.collection('groups').get();
-
     if(groups.empty) {
         res.status(404).json({
             message: 'No groups found'
         });
         return;
     }
-
     const groups_list = [];
     groups.forEach(doc => {
         groups_list.push(doc.data());
@@ -21,7 +19,6 @@ const get_groups = async(req, res) => {
 
 const create_group = async(req, res) => {
     const { name, description, turma } = req.body;
-
     const group = await db.collection('groups').where('name', '==', name).get();
     if(group.docs.length > 0) {
         res.status(400).json({
@@ -29,7 +26,6 @@ const create_group = async(req, res) => {
         });
         return;
     }
-
     const new_group = {
         name,
         description,
@@ -37,7 +33,6 @@ const create_group = async(req, res) => {
         created_at: new Date().toISOString(),
         students: []
     };
-
     try {
         await db.collection('groups').doc(new_group.name).set(new_group);
         res.status(200).json({
@@ -51,7 +46,6 @@ const create_group = async(req, res) => {
 
 const add_student = async(req, res) => {
     const { group_name, student_uuid } = req.body;
-
     const group = await db.collection('groups').doc(group_name).get();
     if(!group.exists) {
         res.status(404).json({
@@ -60,7 +54,6 @@ const add_student = async(req, res) => {
         return;
     }
     const group_data = group.data();
-
     const student = await db.collection('users').doc(student_uuid).get();
     if(!student.exists) {
         res.status(404).json({
@@ -69,7 +62,6 @@ const add_student = async(req, res) => {
         return;
     }
     const student_data = student.data();
-
     if(group_data.students.includes(student_data.uuid)) {
         res.status(400).json({
             message: 'Student already in group'
@@ -77,7 +69,6 @@ const add_student = async(req, res) => {
         return;
     }
     group_data.students.push(student_data.uuid);
-
     try {
         await db.collection('groups').doc(group_name).set(group_data);
         res.status(200).json({
